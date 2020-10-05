f55cc3d7341016f16d73a92abe7f98521d505462
Santhosh Nallasamy <santhosh.nallasamy@Impigertech.com>
2020-10-05 11:37:07 +0530

    initial commit

---
+++ Herbal.yah-varmalayam/Forms/FormBase.cs
@@ -0,0 +1,15 @@
+﻿using Herbal.yah_varmalayam.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Herbal.yah_varmalayam.Forms
+{
+    public class FormBase : Form
+    {
+        protected HerbalEntities herbalContext = new HerbalEntities();
+    }
+}
